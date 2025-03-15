4d90d15277f0081a062306b7cba391df40f7b103
Ganzadonatien <niyocroirealine@gmail.com>
2025-03-15 20:36:37 +0200

    Creating Virtuall fitting Routes

--- server.js
+++ server.js
@@ -20,7 +20,11 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middleware
-app.use(cors())
+app.use(cors({
+  origin: "http://localhost:3000", // Allow frontend requests
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true // Allow cookies & authorization headers
+}));
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ extended: true, limit: "50mb" }))
 
